ee85fcbabc3d4997ea2b9c459b7c8fbfc25f600a
Okwuidegbe Emmanuel <you@example.com>
2020-05-19 04:17:18 +0100

    Develop signup endpoint wrote test for signup endpoint using chai-http/mocha created user model created users route added validations and error handling for user input added jsonwebtoken/bcrypt re-structured code, files and folders
    finishes #172862435

---
+++ server/helpers/authentication/authtoken.js
@@ -0,0 +1,17 @@
+import bcrypt from 'bcrypt';
+import jwt from 'jsonwebtoken';
+
+const authtoken = {
+  hashPassword(password) {
+    return bcrypt.hashSync(password, bcrypt.genSaltSync(10));
+  },
+
+  comparePassword(hashPassword, password) {
+    return bcrypt.compareSync(password, hashPassword);
+  },
+  generateToken(id, isAdmin) {
+    const token = jwt.sign({ userid: id, admin: isAdmin }, process.env.jwtPrivateKey);
+    return token;
+  },
+};
+export default authtoken;
